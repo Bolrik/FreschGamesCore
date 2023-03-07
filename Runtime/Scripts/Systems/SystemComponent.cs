39e1442a07c048cd6e183db7e836466f4579622a
Bolrik <62060123+Bolrik@users.noreply.github.com>
2023-03-07 13:45:39 +0100

    Update SystemComponent.cs

--- Runtime/Scripts/Systems/SystemComponent.cs
+++ Runtime/Scripts/Systems/SystemComponent.cs
@@ -13,31 +13,54 @@ namespace FreschGames.Core.Systems
 
         private void Awake()
         {
+            this.PreAwake();
             this.Manager.DoAwake();
             this.OnAwake();
         }
 
         private void Start()
         {
+            this.PreStart();
             this.Manager.DoStart();
             this.OnStart();
         }
 
         private void Update()
         {
+            this.PreUpdate();
             this.Manager.DoUpdate();
             this.OnUpdate();
         }
 
         private void LateUpdate()
         {
+            this.PreLateUpdate();
             this.Manager.DoLateUpdate();
             this.OnLateUpdate();
         }
 
+        /// <summary>
+        /// Is called, right before the Managers DoAwake is called.
+        /// </summary>
+        protected abstract void PreAwake();
         protected abstract void OnAwake();
+
+        /// <summary>
+        /// Is called, right before the Managers DoStart is called.
+        /// </summary>
+        protected abstract void PreStart();
         protected abstract void OnStart();
+
+        /// <summary>
+        /// Is called, right before the Managers DoUpdate is called.
+        /// </summary>
+        protected abstract void PreUpdate();
         protected abstract void OnUpdate();
+
+        /// <summary>
+        /// Is called, right before the Managers DoLateUpdate is called.
+        /// </summary>
+        protected abstract void PreLateUpdate();
         protected abstract void OnLateUpdate();
     }
 }
