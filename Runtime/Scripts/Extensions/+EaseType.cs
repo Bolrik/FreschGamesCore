a29646b37ee78733b139ecc03d58f7b34a09ccc1
Bolrik <62060123+Bolrik@users.noreply.github.com>
2023-08-13 17:02:38 +0200

    Evalute EaseType

---
+++ Runtime/Scripts/Extensions/+EaseType.cs
@@ -0,0 +1,36 @@
+﻿using FreschGames.Core.Misc.Easing;
+using FreschGames.Core.Misc.Easing.Types;
+
+namespace UnityEngine
+{
+    public static partial class Extension
+    {
+        public static float Evaluate(this EaseType easeType, float t, EaseDirection direction)
+        {
+            switch (easeType)
+            {
+                case EaseType.Linear:
+                    return EaseLinear.Evaluate(t, direction);
+                case EaseType.Quadratic:
+                    return EaseQuadratic.Evaluate(t, direction);
+                case EaseType.Cubic:
+                    return EaseCubic.Evaluate(t, direction);
+                case EaseType.Sine:
+                    return EaseSine.Evaluate(t, direction);
+                case EaseType.Circular:
+                    return EaseCircular.Evaluate(t, direction);
+                case EaseType.Exponential:
+                    return EaseExponential.Evaluate(t, direction);
+                case EaseType.Elastic:
+                    return EaseElastic.Evaluate(t, direction);
+                case EaseType.Back:
+                    return EaseBack.Evaluate(t, direction);
+                case EaseType.Bounce:
+                    return EaseBounce.Evaluate(t, direction);
+
+                default:
+                    return t;
+            }
+        }
+    }
+}
