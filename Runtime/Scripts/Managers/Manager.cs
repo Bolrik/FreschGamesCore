0d691836da65eea93c8ad4c803d42ed1d78c38a9
Bolrik <62060123+Bolrik@users.noreply.github.com>
2024-04-26 14:42:23 +0200

    1.0.4

--- Runtime/Scripts/Managers/Manager.cs
+++ Runtime/Scripts/Managers/Manager.cs
@@ -14,8 +14,6 @@ namespace FreschGames.Core.Managers
 
     public abstract partial class Manager : ScriptableObject
     {
-        [field: SerializeField] public bool IsScenePersistent { get; private set; }
-
         private ManagerHook hook;
         protected ManagerHook Hook
         {
@@ -31,109 +29,82 @@ namespace FreschGames.Core.Managers
             set { hook = value; }
         }
 
-
-        // Called by ManagerSystem.Awake()
-        public virtual void SystemAwake() { }
-
-        // Called by ManagerSystem.Start()
-        public virtual void SystemStart() { }
-
-        // Called by ManagerSystem.Update()
-        public virtual void SystemUpdate() { }
-
-        // Called by ManagerSystem.LateUpdate()
-        public virtual void SystemLateUpdate() { }
-
-        // Called by ManagerSystem.OnEnable()
-        public virtual void SystemEnable() { }
-
-        // Called by ManagerSystem.OnDisable()
-        public virtual void SystemDisable() { }
-
-        // Called as soon as the SceneHook was created
-        protected virtual void OnSceneHookCreated() { }
-
-        protected void CreateSceneHook()
+        public void CreateSceneHook()
         {
-            if (this.IsScenePersistent)
+            if (this.hook == null)
             {
-                if (this.hook == null)
-                {
-                    GameObject hookObject = new GameObject($"{this.GetType().Name} Hook");
-                    this.Hook = hookObject.AddComponent<ManagerHook>();
-                    this.Hook.Set(this);
-
-                    this.OnSceneHookCreated();
-                }
+                GameObject hookObject = new GameObject($"{this.GetType().Name} Hook");
+                this.Hook = hookObject.AddComponent<ManagerHook>();
+                this.Hook.Set(this);
             }
         }
     }
 
-#if UNITY_EDITOR
-    public abstract partial class Manager : ScriptableObject
-    {
-        protected virtual void Awake()
-        {
+//#if UNITY_EDITOR
+//    public abstract partial class Manager : ScriptableObject
+//    {
+//        protected virtual void Awake()
+//        {
 
-        }
+//        }
 
-        protected virtual void OnEnable()
-        {
-            if (Application.IsPlaying(this))
-            {
-                this.CreateSceneHook();
-            }
-            else if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode)
-            {
-                UnityEditor.EditorApplication.playModeStateChanged += this.EditorApplication_PlayModeStateChanged;
+//        protected virtual void OnEnable()
+//        {
+//            if (Application.IsPlaying(this))
+//            {
+//                this.CreateSceneHook();
+//            }
+//            else if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode)
+//            {
+//                UnityEditor.EditorApplication.playModeStateChanged += this.EditorApplication_PlayModeStateChanged;
 
-                return;
-            }
-        }
+//                return;
+//            }
+//        }
 
-        protected virtual void OnDisable()
-        {
+//        protected virtual void OnDisable()
+//        {
 
-        }
+//        }
 
-        protected virtual void OnDestroy()
-        {
+//        protected virtual void OnDestroy()
+//        {
 
-        }
+//        }
 
-        private void EditorApplication_PlayModeStateChanged(UnityEditor.PlayModeStateChange stateChange)
-        {
-            if (stateChange == UnityEditor.PlayModeStateChange.EnteredPlayMode)
-            {
-                UnityEditor.EditorApplication.playModeStateChanged -= this.EditorApplication_PlayModeStateChanged;
-                this.CreateSceneHook();
-            }
-        }
-    }
-#endif
+//        private void EditorApplication_PlayModeStateChanged(UnityEditor.PlayModeStateChange stateChange)
+//        {
+//            if (stateChange == UnityEditor.PlayModeStateChange.EnteredPlayMode)
+//            {
+//                UnityEditor.EditorApplication.playModeStateChanged -= this.EditorApplication_PlayModeStateChanged;
+//                this.CreateSceneHook();
+//            }
+//        }
+//    }
+//#endif
 
-#if !UNITY_EDITOR
-    public abstract partial class Manager : ScriptableObject
-    {
-        protected virtual void Awake()
-        {
+//#if !UNITY_EDITOR
+//    public abstract partial class Manager : ScriptableObject
+//    {
+//        protected virtual void Awake()
+//        {
 
-        }
+//        }
 
-        protected virtual void OnEnable()
-        {
-            this.CreateSceneHook();
-        }
+//        protected virtual void OnEnable()
+//        {
+//            this.CreateSceneHook();
+//        }
 
-        protected virtual void OnDisable()
-        {
+//        protected virtual void OnDisable()
+//        {
 
-        }
+//        }
 
-        protected virtual void OnDestroy()
-        {
+//        protected virtual void OnDestroy()
+//        {
 
-        }
-    }
-#endif
+//        }
+//    }
+//#endif
 }
